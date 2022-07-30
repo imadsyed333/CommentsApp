28148d8d2bfcd1e0392f88168ad01a8936ac1308
Imad <creeperone23@gmail.com>
2022-07-30 12:24:04 -0400

    Added commenting functionality

--- components/CommentList.tsx
+++ components/CommentList.tsx
@@ -1,27 +1,73 @@
-import { FlatList, View, ListRenderItem, StyleSheet } from 'react-native';
-import React from 'react';
+import { FlatList, View, ListRenderItem, StyleSheet, TextInput, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 import Comment from './Comment'
 
+type comment = {
+    content: string
+    comments: comment[]
+}
+
 export default function CommentList(props: any) {
+    const [comment, setComment] = useState<string>("")
+    const [comments, setComments] = useState<comment[]>([])
+
     const renderItem: ListRenderItem<any>= ({item} : any) => {
         return(
-            <Comment comment={item.content} comments={item.comments}/>
+            <Comment content={item.content} comments={item.comments} textHint="Comment on this"/>
         )
     }
 
+    const addComment = () => {
+        const new_comment = {
+            content: comment,
+            comments: []
+        }
+        setComments([...comments, new_comment])
+        setComment("")
+    }
+
     return(
-        <FlatList 
-        data={props.comments} 
-        renderItem={renderItem}>
-        </FlatList>
+        <View style={styles.container}>
+            <View style={styles.input_container}>
+                <TextInput onChangeText={setComment} value={comment} style={styles.input} placeholder={props.textHint} placeholderTextColor={"gray"}/>
+                <TouchableOpacity style={styles.button} onPress={addComment}>
+                    <Text>Enter</Text>
+                </TouchableOpacity>
+            </View>
+            <FlatList 
+            data={comments} 
+            renderItem={renderItem}>
+            </FlatList>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#000',
+      backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
     },
+
+    input_container: {
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent: 'center',
+        padding: 2,
+    },
+
+    input: {
+        borderWidth:3, 
+        padding: 5, 
+        borderRadius: 10
+    },
+
+    button: {
+        alignSelf:'flex-end',
+        alignContent:'flex-end',
+        borderWidth: 3,
+        borderRadius:10,
+        padding: 5, 
+    }
   });
