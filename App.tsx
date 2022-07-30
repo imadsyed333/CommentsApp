6123ea0d446b18a63c36ef24703eceb7ff3c32eb
Imad <creeperone23@gmail.com>
2022-07-30 01:02:21 -0400

    Added components for main comment list

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import CommentList from './components/CommentList';
+
+type Comment = {
+  content: string
+  comments: Comment[]
+}
 
 export default function App() {
+  const [comments, setComments] = useState<Comment[]>([])
+  const [comment, setComment] = useState<string>("")
+
+  const addComment = () => {
+    const new_comment = {
+      content: comment, 
+      comments: []
+    }
+    setComments([...comments, new_comment])
+    setComment("")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>COMMENTS!</Text>
+      <TextInput onChangeText={setComment} value={comment}/>
+      <Button title='Add Comment' onPress={addComment}/>
+      <View></View>
+      <CommentList comments={comments}/>
     </View>
   );
 }
